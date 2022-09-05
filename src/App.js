4d60ba5253fcdd0aba3fdcfd6c0dec51d3916188
panasonikk <grebenyuk1507@gmail.com>
2022-09-05 09:47:11 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">Hello</div>
+    </div>
+  );
+}
